53d934a7c6191ef842a7d6716b8b2a3fde2b60ae
Aviral235 <aviral.020305@gmail.com>
2026-02-12 14:56:18 +0530

    added new components

---
+++ front/src/Components/VideoPreview.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+
+ * 
+ * @param {Object} props - Component props
+ * @param {ReactNode} props.children - Child elements to be rendered within the component
+ * @returns {ReactNode} The children passed to this component
+ */
+const VideoPreview = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default VideoPreview;
